e6ebc13878fef16ab6aff72689fa2cb47bae1b94
Sho0226 <s1F102201616@iniad.org>
2024-10-15 02:40:57 +0900

    add: Header file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import { Header } from '../components/model/header/header';
 import styles from './index.module.css';
 
 const Home = () => {
   return (
     <div className={styles.container}>
+      <Header />
       <h1>
         Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
